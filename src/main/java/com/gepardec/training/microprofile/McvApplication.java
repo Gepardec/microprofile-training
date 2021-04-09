e00eadbc4b6e53176a99c6457d5c303024da60e8
cchet <thomas.herzog@gepardec.com>
2021-04-09 19:15:09 +0200

    - Added mdb 5 - Implemented some common components via tags - Implemented layout facelet - Implemented welcome and index page skeletons

--- src/main/java/com/gepardec/training/microprofile/McvApplication.java
+++ src/main/java/com/gepardec/training/microprofile/McvApplication.java
@@ -3,7 +3,7 @@ package com.gepardec.training.microprofile;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/")
+@ApplicationPath("/api")
 public class McvApplication extends Application {
 
 }
