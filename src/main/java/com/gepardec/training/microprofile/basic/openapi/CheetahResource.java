3abd5e1cda55ef43bd46cad590880444b08fe6b2
cdjri <johannes.riegler@gepardec.com>
2021-07-06 13:20:15 +0200

    annotations task

--- src/main/java/com/gepardec/training/microprofile/basic/openapi/CheetahResource.java
+++ src/main/java/com/gepardec/training/microprofile/basic/openapi/CheetahResource.java
@@ -1,12 +1,19 @@
 package com.gepardec.training.microprofile.basic.openapi;
 
+import jdk.jfr.Description;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Set;
-
 @Path("/cheetahs")
 public class CheetahResource {
 
@@ -18,6 +25,10 @@ public class CheetahResource {
     }
 
     @Produces(MediaType.APPLICATION_JSON)
+//    @Schema
+//    @APIResponse
+//    @Parameter
+//    @Operation
     @GET
     public Set<Cheetah> list() {
         return cheetahs;
@@ -32,6 +43,10 @@ public class CheetahResource {
 
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
+//    @APIResponse(
+//            responseCode = "201",
+//            description = "Cheetah created"
+//    )
     @POST
     public Set<Cheetah> add(Cheetah cheetah) {
         cheetahs.add(cheetah);
@@ -44,6 +59,24 @@ public class CheetahResource {
     public Set<Cheetah> delete(Cheetah cheetah) {
         cheetahs.removeIf(existingCheetah -> existingCheetah.getName().equalsIgnoreCase(cheetah.getName()));
         return cheetahs;
+    }
+
+//    @Produces(MediaType.APPLICATION_JSON)
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Description("This service is for testing error responses")
+//    @Operation(
+//            description = "Response will be an internal error server or noContent")
+//    @APIResponse(
+//            name = "internalError",
+//            responseCode = "204",
+//            description = "No Cheetah found"
+//    )
+    @DELETE
+    public Response error(Cheetah cheetah) {
+        if(cheetah == null){
+            return Response.noContent().build();
+        }
+        return Response.serverError().build();
     }
 
 }
