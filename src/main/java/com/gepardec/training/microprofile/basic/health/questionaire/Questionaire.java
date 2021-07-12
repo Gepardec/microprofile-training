bb089bb6d0ea2a7e4fdd68ddc02d072cf63ac6ff
Mario Oberwalder <mario.oberwalder@gepardec.com>
2021-07-12 16:37:41 +0200

    better names

--- src/main/java/com/gepardec/training/microprofile/basic/health/questionaire/Questionaire.java
+++ src/main/java/com/gepardec/training/microprofile/basic/health/questionaire/Questionaire.java
@@ -8,21 +8,30 @@ public class Questionaire {
 
     public Questionaire() {
         var responsesAndAnswers = new HashMap<Integer, String>();
-        responsesAndAnswers.put(1, "Wildfly uses one port for server interactions and another for the deployed application. Per specification the health status is \"DOWN\" when a single HealthCheck fails. So this makes some sense and is actually okay.");
+        responsesAndAnswers.put(1, "Our Wildfly is configured to use one port for the server stuff and one for the application. " +
+                "Per specification the health status is \"DOWN\" when a single HealthCheck fails. " +
+                "So this makes some sense and is actually okay.");
         responsesAndAnswers.put(2, "Actually everything seems to be working as intended.");
         questions.put(1,
                       new Question<>("Is this okay?",
                                      responsesAndAnswers));
 
         responsesAndAnswers = new HashMap<>();
-        responsesAndAnswers.put(1, "YesYes!");
+        responsesAndAnswers.put(1, "No");
         responsesAndAnswers.put(2, "NoNo");
         questions.put(2,
-                      new Question<>("Does this still work?",
+                      new Question<>("In theory we could disable the default healthchecks, its bugged right now",
+                                     responsesAndAnswers));
+
+        responsesAndAnswers = new HashMap<>();
+        responsesAndAnswers.put(1, "Works");
+        responsesAndAnswers.put(2, "Seems broken");
+        questions.put(3,
+                      new Question<>("One healtchcheck is \"DOWN\" fix it",
                                      responsesAndAnswers));
     }
 
-    public String getQuestion(Integer questionKey) {
+    public String getQuestionText(Integer questionKey) {
         return questions.get(questionKey).getQuestionText();
     }
 
