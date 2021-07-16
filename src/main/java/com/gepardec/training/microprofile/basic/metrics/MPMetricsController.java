c316f9e50876ba2072c41c459e9c2ceca9959483
Sabrina <sabrina.ploner@gepardec.com>
2021-07-16 12:26:50 +0200

    #7 add example for @Counted

--- src/main/java/com/gepardec/training/microprofile/basic/metrics/MPMetricsController.java
+++ src/main/java/com/gepardec/training/microprofile/basic/metrics/MPMetricsController.java
@@ -1,7 +1,12 @@
 package com.gepardec.training.microprofile.basic.metrics;
 
+import org.eclipse.microprofile.metrics.Counter;
+import org.eclipse.microprofile.metrics.MetricRegistry;
+
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.mvc.Controller;
+import javax.mvc.Models;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -10,9 +15,30 @@ import javax.ws.rs.Path;
 @Controller
 public class MPMetricsController {
 
+    @Inject
+    private MetricRegistry metricRegistry;
+
+    @Inject
+    private Models model;
+
     @Path("/enable")
     @GET
     public String isEnabled(){
         return "basic/metrics/enable.xhtml";
     }
+
+    @Path("/counted")
+    @GET
+    public String getCounted(){
+        Counter change = metricRegistry.counter("count-example");
+        model.put("count", change.getCount());
+        return "basic/metrics/counted.xhtml";
+    }
+
+    @Path("/count")
+    @GET
+    public String count(){
+        return getCounted();
+    }
+
 }
