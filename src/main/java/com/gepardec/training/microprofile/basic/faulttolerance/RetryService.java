374ece0ba04bb8ca93ea531695ee4a9cfb973ae8
Thomas Herzog <herzog.thomas81@gmail.com>
2021-08-10 20:32:53 +0200

    - Some refactorings necessary because of changes made on main branch - Removed already implemented examples which where for testing

--- src/main/java/com/gepardec/training/microprofile/basic/faulttolerance/RetryService.java
+++ src/main/java/com/gepardec/training/microprofile/basic/faulttolerance/RetryService.java
@@ -10,7 +10,6 @@ public class RetryService {
 
     private int counter = 0;
 
-    @Retry(maxRetries = 3, delay = 1500L, delayUnit = ChronoUnit.MILLIS)
     public int retried() {
         counter++;
         if (counter < 3) {
