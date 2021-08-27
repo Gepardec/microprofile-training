95767f2755b792fee0a82e7ec07dc71441e872e4
Sabrina <sabrina.ploner@gepardec.com>
2021-08-27 09:00:38 +0200

    #1 example for common configuration

---
+++ config/common-configuration/src/main/java/com/gepardec/training/microprofile/common/ExampleController.java
@@ -0,0 +1,31 @@
+package com.gepardec.training.microprofile.common;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.mvc.Controller;
+import javax.mvc.Models;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/advanced/config")
+@RequestScoped
+@Controller
+public class ExampleController {
+
+    @Inject
+    private Models model;
+
+    @Inject
+    @ConfigProperty(name = "common.key")
+    private String commonConfigurationValue;
+
+    @Path("/commonconfiguration")
+    @GET
+    public String getConfigFromPropertyFile() {
+        model.put("value", commonConfigurationValue);
+        return "advanced/config/common_configuration.xhtml";
+    }
+
+}
