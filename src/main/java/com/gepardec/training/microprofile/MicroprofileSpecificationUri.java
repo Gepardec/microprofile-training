371f287b4e421c267619dac45f567470fd730f51
cchet <thomas.herzog@gepardec.com>
2021-10-02 10:01:16 +0200

    - Switched to mpUri bean for used doc uris for MP-Config in views

--- src/main/java/com/gepardec/training/microprofile/MicroprofileSpecificationUri.java
+++ src/main/java/com/gepardec/training/microprofile/MicroprofileSpecificationUri.java
@@ -7,15 +7,22 @@ import javax.inject.Named;
 @ApplicationScoped
 public class MicroprofileSpecificationUri {
 
-    private final String faultToleranceUri = "https://download.eclipse.org/microprofile/microprofile-fault-tolerance-3.0/microprofile-fault-tolerance-spec-3.0.html";
+    private static final String FAULT_TOLERANCE_URI = "https://download.eclipse.org/microprofile/microprofile-fault-tolerance-3.0/microprofile-fault-tolerance-spec-3.0.html";
 
-    private final String jwtUri = "https://download.eclipse.org/microprofile/microprofile-jwt-auth-1.2/microprofile-jwt-auth-spec-1.2.html";
+    private static final String JWT_URI = "https://download.eclipse.org/microprofile/microprofile-jwt-auth-1.2/microprofile-jwt-auth-spec-1.2.html";
+
+    private static final String CONFIG_URI = "https://download.eclipse.org/microprofile/microprofile-config-2.0/microprofile-config-spec-2.0.html";
 
     public String getFaultToleranceUri() {
-        return faultToleranceUri;
+        return FAULT_TOLERANCE_URI;
     }
 
     public String getJwtUri() {
-        return jwtUri;
+        return JWT_URI;
     }
+
+    public String getConfigUri() {
+        return CONFIG_URI;
+    }
+
 }
