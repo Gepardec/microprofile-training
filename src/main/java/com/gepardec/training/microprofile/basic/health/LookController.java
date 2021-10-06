01080b60857af9f392f8d3478569a0b20fcd117b
Mario Oberwalder <mario.oberwalder@gepardec.com>
2021-10-06 17:43:37 +0200

    reworked first look at health endpoint example

---
+++ src/main/java/com/gepardec/training/microprofile/basic/health/LookController.java
@@ -0,0 +1,18 @@
+package com.gepardec.training.microprofile.basic.health;
+
+import javax.enterprise.context.RequestScoped;
+import javax.mvc.Controller;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/basic/health/look")
+@RequestScoped
+public class LookController {
+
+    @Path("/")
+    @GET
+    @Controller
+    public String getConfigFromPropertyFile() {
+        return "basic/health/look.xhtml";
+    }
+}
