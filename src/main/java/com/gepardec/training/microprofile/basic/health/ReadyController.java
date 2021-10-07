ac0542ae94d69ff36201836b469acd7d0ae406d8
Mario Oberwalder <mario.oberwalder@gepardec.com>
2021-10-07 10:44:39 +0200

    added logic structure

--- src/main/java/com/gepardec/training/microprofile/basic/health/ReadyController.java
+++ src/main/java/com/gepardec/training/microprofile/basic/health/ReadyController.java
@@ -1,7 +1,9 @@
 package com.gepardec.training.microprofile.basic.health;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.mvc.Controller;
+import javax.mvc.Models;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -9,10 +11,22 @@ import javax.ws.rs.Path;
 @RequestScoped
 public class ReadyController {
 
+    @Inject
+    private Models model;
+
     @Path("/")
     @GET
     @Controller
     public String getReady() {
+        if (getHealthCheckStateByName("FixMe", "Readiness")) {
+            model.put("State", "UP");
+        } else {
+            model.put("State", "DOWN");
+        }
         return "basic/health/ready.xhtml";
+    }
+
+    private boolean getHealthCheckStateByName(String fixMe, String readiness) {
+        return true;
     }
 }
