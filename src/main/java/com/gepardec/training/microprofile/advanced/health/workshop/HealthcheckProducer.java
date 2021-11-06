5955b695fbde62094c8f2da6063d14eb9c808055
Mario Oberwalder <mario.oberwalder@gepardec.com>
2021-11-06 10:33:56 +0100

    removed empy producer template

--- src/main/java/com/gepardec/training/microprofile/advanced/health/workshop/HealthcheckProducer.java
+++
@@ -1,8 +0,0 @@
-package com.gepardec.training.microprofile.advanced.health.workshop;
-
-import javax.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class HealthcheckProducer {
-
-}
