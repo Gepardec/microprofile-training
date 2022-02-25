491c3de1703a02d7691227cea6802597372b3b3a
cchet <thomas.herzog@gepardec.com>
2022-02-25 08:57:19 +0100

    - Merged configuration class holding configuration map with config source

--- config/drop-in-configuration/src/main/java/com/gepardec/training/microprofile/dropinconfiguration/DropInConfigurationConfigSource.java
+++ config/drop-in-configuration/src/main/java/com/gepardec/training/microprofile/dropinconfiguration/DropInConfigurationConfigSource.java
@@ -2,18 +2,28 @@ package com.gepardec.training.microprofile.dropinconfiguration;
 
 import org.eclipse.microprofile.config.spi.ConfigSource;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class DropInConfigurationConfigSource implements ConfigSource {
 
+    private static final Map<String, String> properties = new HashMap<>();
+
+    static {
+        properties.put("drop.in.key1", "Value 1 from DropInConfiguration");
+        properties.put("drop.in.key2", "Value 2 from DropInConfiguration");
+        properties.put("drop.in.key3", "Value 3 from DropInConfiguration");
+    }
+
     @Override
     public Set<String> getPropertyNames() {
-        return DropInConfiguration.getProperties().keySet();
+        return properties.keySet();
     }
 
     @Override
     public String getValue(String propertyName) {
-        return DropInConfiguration.getProperties().get(propertyName);
+        return properties.get(propertyName);
     }
 
     @Override
