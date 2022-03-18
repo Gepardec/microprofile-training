fd11a9db958ec19e4e4d921112b71c3a01053ba6
cchet <thomas.herzog@gepardec.com>
2022-03-18 15:18:23 +0100

    - startup not necessary is application scoped anyway

--- src/main/java/com/gepardec/training/microprofile/basic/metrics/RandomNumberGenerator.java
+++ src/main/java/com/gepardec/training/microprofile/basic/metrics/RandomNumberGenerator.java
@@ -1,11 +1,8 @@
 package com.gepardec.training.microprofile.basic.metrics;
 
-import org.omnifaces.cdi.Startup;
-
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-@Startup
 public class RandomNumberGenerator {
 
     public double getValue() {
